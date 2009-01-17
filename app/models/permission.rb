114e0853cd6d898e9fd0c7b97aa8b53cb0148bdf
neilmcg <neilmcgeehan@neil-mcgeehans-macbook-pro.local>
2009-01-17 13:55:56 -0500

    not working...

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,4 @@
 class Permission < ActiveRecord::Base
-  belongs_to :user, :foreign_key => "owner_id"
+  belongs_to :user
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :permissions
-
   map.resources :users
   map.resource :session
+  map.resources :permissions
   
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
