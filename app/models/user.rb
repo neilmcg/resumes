f7eb591b5e3dbfb902169bfb59cd20eb2b3a01e1
Pat Nakajima <patnakajima@gmail.com>
2009-01-17 14:41:54 -0500

    added proper foreign_key option to user permissions association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'digest/sha1'
 class User < ActiveRecord::Base
   include AuthenticatedModel # moved this logic to lib/authenticated_model.rb
   
-  has_many :permissions
+  has_many :permissions, :foreign_key => :owner_id
   
   def can_view_profile?(user_with_profile)
     false
